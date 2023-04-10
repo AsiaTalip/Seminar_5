f5eb26913cbf95726e93d6bd6169e81c4fa75fdf
Asia <asiaverner666@gmail.com>
2023-04-10 15:29:59 +0300

    Задание 3

---
+++ Задание_3/Program.cs
@@ -0,0 +1,21 @@
+﻿double[] arr = new double[] { 3, 7, 22, 2, 78 };
+
+double max = arr[0];
+double min = arr[0];
+
+foreach (double num in arr)
+{
+    if (num > max)
+    {
+        max = num;
+    }
+
+    if (num < min)
+    {
+        min = num;
+    }
+}
+
+double diff = max - min;
+
+Console.WriteLine(diff);
